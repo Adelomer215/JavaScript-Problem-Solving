1b73c114a7ca26d1617de9e6f4775ef325a6bc8d
Adel <adelimer5533@gmail.com>
2023-02-21 02:04:39 +0200

    solve the problem successfully

---
+++ EvenOrAdd.js
@@ -0,0 +1,13 @@
+/*
+    First problem 
+    Even or Odd from codewars
+    link -> https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
+        - Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+
+*/
+
+function evenOrOdd(number) {
+  return number % 2 === 0 ? "Even" : "Odd";
+}
+console.log(evenOrOdd(21));
