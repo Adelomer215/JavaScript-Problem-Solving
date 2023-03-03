202fe3228536ca00c2683fe7c253e4a9b65a69e6
Adel <adelimer5533@gmail.com>
2023-03-03 16:10:12 +0200

    problem solved

---
+++ Stringrepeat.js
@@ -0,0 +1,19 @@
+/*
+Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+Examples (input -> output)
+6, "I"     -> "IIIIII"
+5, "Hello" -> "HelloHelloHelloHelloHello"
+
+link -> https://www.codewars.com/kata/57a0e5c372292dd76d000d7e/train/javascript
+*/
+
+function repeatStr(n, s) {
+  let str = "";
+  for (let i = 0; i < n; i++) {
+    str += s;
+  }
+  console.log(str);
+  return str;
+}
+repeatStr(15, "a");
